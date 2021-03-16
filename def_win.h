d171b3bb782504d62b23cb4b190ef5a7d2e64949
xikavi <xtzy.xx@gmail.com>
2021-03-16 22:56:11 +0300

    Nice HRESULT error string & code output

--- def_win.h
+++ def_win.h
@@ -2,7 +2,7 @@
 #define DEF_WIN_H
 #include "log.h"
 
-#define SAFE_RELEASE(punk) if ((punk) != NULL) { (punk)->Release(); (punk) = NULL; }
-#define EXIT_ON_ERROR(hres) if (FAILED(hres)) { FileDebug() << "HRESULT FAILED " << hres; goto Exit; }
+#define SAFE_RELEASE(punk) if ((punk) != nullptr) { (punk)->Release(); (punk) = nullptr; }
+#define EXIT_ON_ERROR(hres) if (FAILED(hres)) { FileDebug() << "HRESULT FAILED " << Qt::hex << HRESULT_CODE(hres) << HResultToString(hres); goto Exit; }
 
 #endif // DEF_WIN_H
