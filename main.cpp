021b96c05132b42c80afd53517e5f31fcb148b59
xikavi <xtzy.xx@gmail.com>
2021-03-16 23:02:54 +0300

    Windows media foundation startup & shutdown code

--- main.cpp
+++ main.cpp
@@ -1,14 +1,17 @@
 #include <QApplication>
+#include <mfapi.h>
 #include "eventfilter.h"
 #include "mainwindow.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    MFStartup(MF_VERSION);
     EventFilter nativeEventFilter;
     a.installNativeEventFilter(&nativeEventFilter);
     MainWindow w;
     QObject::connect(&nativeEventFilter, &EventFilter::systemResumed, &w, &MainWindow::onSystemResumed);
     auto result = a.exec();
+    MFShutdown();
     return result;
 }
