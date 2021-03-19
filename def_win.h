169d469c19ca1b58f9d980b508acea84c0cb2659
xikavi <xtzy.xx@gmail.com>
2021-03-19 04:39:31 +0300

    EXIT_ON_ERROR define improve
    Printing detailed information about error location

--- def_win.h
+++ def_win.h
@@ -3,6 +3,9 @@
 #include "log.h"
 
 #define SAFE_RELEASE(punk) if ((punk) != nullptr) { (punk)->Release(); (punk) = nullptr; }
-#define EXIT_ON_ERROR(hres) if (FAILED(hres)) { FileDebug() << "HRESULT FAILED " << Qt::hex << HRESULT_CODE(hres) << HResultToString(hres); goto Exit; }
+
+#define EXIT_ON_ERROR(hres) if (FAILED(hres)) { FileDebug() \
+    << "In" << __FILE__ << "line" << __LINE__ << "func" << __func__ \
+    << "HRESULT failed with code" << Qt::hex << HRESULT_CODE(hres) << HResultToString(hres); goto Exit; }
 
 #endif // DEF_WIN_H
